cf0fcdeac46a1c030a0a4e294b0bfcaba1f22de1
范哲源 <fanzheyuan@le.com>
2017-08-11 17:17:10 +0800

    edit python file

--- python/app.py
+++ python/app.py
@@ -1,22 +1,24 @@
-from flask import Flask
-from redis import Redis, RedisError
+from flask
+import Flask
+from redis
+import Redis, RedisError
 import os
 import socket
 
 app = Flask(__name__)
 
-@app.route("/")
+@ app.route("/")
 def hello():
- try:
- 	visits = redis.incr("counter")
- except RedisError:
- 	visits = "<i>cannot connect to Redis, counter disabled</i>"
+    try:
+    visits = redis.incr("counter")
+except RedisError:
+    visits = "<i>cannot connect to Redis, counter disabled</i>"
 
- html = "<h3>Hello {name}!</h3>" \
- "<b>Hostname:</b> {hostname}<br/>" \
- "<b>Visits:</b> {visits}"
+html = "<h3>Hello {name}!</h3>"\
+"<b>Hostname:</b> {hostname}<br/>"\
+"<b>Visits:</b> {visits}"
 
- return html.format(name=os.getenv("NAME","world"),hostname=socket.gethostname(),visits=visits)
+return html.format(name = os.getenv("NAME", "world"), hostname = socket.gethostname(), visits = visits)
 
- if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=80)
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 80)
