c2635b5654908f9df38f703eaf3e7d2fa61f4846
范哲源 <fanzheyuan@le.com>
2017-08-14 13:08:22 +0800

    add redis init code

--- python/app.py
+++ python/app.py
@@ -2,24 +2,12 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask
-from redis import Redis, RedisError
-import os
-import socket
-
-# Connect to Redis
-redis = Redis(host = "redis", db = 0, socket_connect_timeout = 2, socket_timeout = 2)
 
 app = Flask(__name__)
 
-@ app.route("/")
+@app.route("/")
 def hello():
- visits = redis.incr("counter")
-
-html = "<h3>Hello {name}!</h3>"\
-"<b>Hostname:</b> {hostname}<br/>"\
-"<b>Visits:</b> {visits}"
-
-return html.format(name = os.getenv("NAME", "world"), hostname = socket.gethostname(), visits = visits)
+ return 'Hello World!'
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 80)
